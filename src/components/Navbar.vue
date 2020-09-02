0f39a864ca6fbd74ef67642bdc8f44f0fa7ce247
mr-zero <amirajubolchi001@gmail.com>
2020-09-02 21:03:32 +0530

    Modified files

---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="navbar">
+        <nav class="nav-extended indigo darken-2">
+            <div class="nav-content">
+                <router-link to="">
+                    <span class="nav-title">Ninja Cocktail</span>
+                </router-link>
+                <a href="" class="btn-floating btn-large halfway-fab pink">
+                    <router-link to="">
+                    <i class="material-icons">add</i>
+                    </router-link>
+                </a>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Navbar",
+    props: {},
+};
+</script>
+
+<style scoped>
+.navbar nav{
+    padding: 0 20px;
+}
+</style>
